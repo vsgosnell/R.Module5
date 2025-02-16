aa3fcb52d0f8a81f076f7b25610a3f89646a0270
Veronica Gosnell <vsgosnell@usf.edu>
2025-02-16 15:44:09 -0500

    initial commit

---
+++ inverse.matrices.pdf.R
@@ -0,0 +1,29 @@
+# The Inverse of a Matrix (pdf)
+# The inverse of a square n × n matrix A, is another n × n matrix denoted by A−1 such that
+# AA^−1= A^−1A= I
+# where I is the n × n identit y matrix. 
+# That is, multiplying a matrix by its inverse produces an identit y matrix. 
+# Not all square matrices have an inverse matrix. 
+# If the determinant of the matrix is zero, then it will not have an inverse, 
+# and the matrix is said to be singular. 
+# Only non-singular matrices have inverses.
+
+
+
+# Given any non-singular matrix A, its inverse can be found from the formula
+# A^−1 = adj A/|A|
+# where adj A is the adjoint matrix and |A| is the determinan t of A. 
+# The procedure for finding the adjoint matrix is given below.
+
+
+
+
+
+#this pdf breaks down the procedure to understand the inverse operations of matrices
+#it does not quite make sense to me
+
+
+
+
+
+
